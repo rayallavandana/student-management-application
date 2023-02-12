2930541f42d1fbf81af48c4ac1db83eb51849b60
rayallavandana <rayallavandana10@gmail.com>
2023-02-12 11:14:12 +0530

    printing the arguments

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+        public static void main(String[] args){
+            if (args.length == 1){
+                System.out.println(args[0]);
+            }
+            else{
+                System.out.println("no arguments given");
+            }
+
+        }
+        
+}
